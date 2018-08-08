98181a0bbafda60852ebde569c30aa5d909b02ad
Qi Wang <qiwan@redhat.com>
2018-08-08 14:08:28 -0400

    test del

--- src/ContainerDeleteModal.jsx
+++ src/ContainerDeleteModal.jsx
@@ -9,7 +9,7 @@ const ContainerDeleteModal = (props) => {
 	return (
 		<div>
 			<Modal isOpen={props.selectContainerDeleteModal} fade={false} >
-				<ModalHeader>
+				<ModalHeader >
 					{cockpit.format(_("Please confirm deletion of $0"), utils.truncate_id(props.containerWillDelete.ID))}
 				</ModalHeader>
 				<ModalBody>
@@ -17,7 +17,7 @@ const ContainerDeleteModal = (props) => {
 				</ModalBody>
 				<ModalFooter>
 					<Button color="secondary" onClick={props.handleCancelContainerDeleteModal}>Cancel</Button>
-					<Button color="danger" onClick={props.handleRemoveContainer}>{_("Delete")}</Button>{' '}
+					<Button id="container-delete-toggle-btn" color="danger" onClick={props.handleRemoveContainer}>{_("Delete")}</Button>{' '}
 				</ModalFooter>
 			</Modal>
 		</div>
